93302088cb26eef1431e5e303f09f39129932872
agerockhub <agerock2013@gmail.com>
2026-07-13 15:02:29 +0000

    Completed W02 Dynamic Product List

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,30 +1,15 @@
-// wrapper for querySelector...returns matching element
-export function qs(selector, parent = document) {
-  return parent.querySelector(selector);
-}
-// or a more concise version if you are into that sort of thing:
-// export const qs = (selector, parent = document) => parent.querySelector(selector);
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false,
+) {
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
 
-// retrieve data from localstorage
-export function getLocalStorage(key) {
-  return JSON.parse(localStorage.getItem(key));
-}
-// save data to local storage
-export function setLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
-}
-// set a listener for both touchend and click
-export function setClick(selector, callback) {
-  qs(selector).addEventListener("touchend", (event) => {
-    event.preventDefault();
-    callback();
-  });
-  qs(selector).addEventListener("click", callback);
-}
+  const htmlStrings = list.map(templateFn);
 
-export function getParam(param) {
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  const product = urlParams.get(param);
-  return product;
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import ProductData from "./js/ProductData.mjs";
+import ProductList from "./js/ProductList.mjs";
+
+const dataSource = new ProductData("tents");
+
+const listElement = document.querySelector(".product-list");
+
+const productList = new ProductList("tents", dataSource, listElement);
+
+productList.init();
