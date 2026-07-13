b922fb2b3b5319b9dcff09220f774cc27b3dd90d
agerockhub <agerock2013@gmail.com>
2026-07-13 17:39:03 +0000

    Fix utils exports and build errors

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,3 +1,11 @@
+export function getLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+
+export function setLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
 export function renderListWithTemplate(
   templateFn,
   parentElement,
