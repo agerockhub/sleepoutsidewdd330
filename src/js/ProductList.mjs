f60c6956d70102ccaa1f1ce11a6c42ea4c40d0d4
agerockhub <agerock2013@gmail.com>
2026-07-14 12:27:25 +0000

    Fix product image paths

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,13 +1,10 @@
-import { renderListWithTemplate } from "./utils.mjs";
+import { renderListWithTemplate } from './utils.mjs';
 
 function productCardTemplate(product) {
   return `
     <li class="product-card">
       <a href="product_pages/?product=${product.Id}">
-        <img
-          src="${product.Image}"
-          alt="${product.NameWithoutBrand}"
-        />
+      <img src="${product.Image.replace('../', '')}" alt="Image of ${product.Name}">
         <h3 class="card__brand">${product.Brand.Name}</h3>
         <h2 class="card__name">${product.NameWithoutBrand}</h2>
         <p class="product-card__price">$${product.FinalPrice}</p>
